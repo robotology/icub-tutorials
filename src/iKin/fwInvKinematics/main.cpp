9453afcc90bc3804b680ea8044850892d7714b9a
Ugo Pattacini <ugo.pattacini@iit.it>
2012-11-29 17:14:54 +0000

    [tutorials]: just comments
    svn path=/trunk/iCub/tutorials/; revision=21679

--- src/iKin/fwInvKinematics/main.cpp
+++ src/iKin/fwInvKinematics/main.cpp
@@ -60,7 +60,7 @@ protected:
         setH0(H0);
 
         // define the links in standard D-H convention
-        //                                A,        D,     alpha,           offset(*),          min theta,          max theta
+        //                             A,        D,     alpha,           offset(*),          min theta,          max theta
         pushLink(new iKinLink(     0.032,      0.0,  M_PI/2.0,                 0.0, -22.0*CTRL_DEG2RAD,  84.0*CTRL_DEG2RAD));
         pushLink(new iKinLink(       0.0,  -0.0055,  M_PI/2.0,           -M_PI/2.0, -39.0*CTRL_DEG2RAD,  39.0*CTRL_DEG2RAD));
         pushLink(new iKinLink(-0.0233647,  -0.1433,  M_PI/2.0, -105.0*CTRL_DEG2RAD, -59.0*CTRL_DEG2RAD,  59.0*CTRL_DEG2RAD));
